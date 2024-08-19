db5111e8df2adb28d93ca90370dc86fe245ae6c0
ziyad <ziyadmuhammad9696@gmail.com>
2024-08-19 08:22:28 +0530

    Rverse Order

---
+++ ReverseOrder.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+    int limit,array[100],i,position,num,j;
+
+    printf("enter limit\n");
+    scanf("%d",&limit);
+
+    printf("enter your values\n");
+      for(i=0;i<limit;i++){
+       scanf("%d",&array[i]);
+          
+        }
+       for(i=limit-1;i>=0;i--){
+        
+        printf("%d\t",array[i]);
+
+        
+       }
+
+
+      
+       
+       
+       
+ return 0;
+}
