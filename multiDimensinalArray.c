c144bc3bb7bbc221ff16523e17143a929a6481b9
ziyad <ziyadmuhammad9696@gmail.com>
2024-08-29 08:22:03 +0530

    MultiDimensial Array

---
+++ multiDimensinalArray.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int values[3][3],i,j,limit;
+
+    printf("enter limit");
+    scanf("%d",&limit);
+
+   printf("enter your values");
+
+   for(i=0;i<limit;i++){
+    for(j=0;j<limit;j++){
+        scanf("%d",&values[i][j]);
+    }
+   }printf("you have endred values : \n");
+
+   for(i=0;i<limit;i++){
+    for(j=0;j<limit;j++){
+        printf("%d\t",values[i][j]);
+
+    }printf("\n");
+   }
+
+    return 0;
+}
