6cb4f47652db050b204b81c14d9da8243baebe6b
ziyad <ziyadmuhammad9696@gmail.com>
2024-11-29 06:44:44 +0530

    manualy reverse order in array

--- Array/ReverseOrder.c
+++ Array/ReverseOrder.c
@@ -1,30 +1,33 @@
+// Manualy Revers order
+
 
 #include <stdio.h>
 
 int main()
 {
 
-    int limit,array[100],i,position,num,j;
+  int limit, array[100], i, temp = 0;
 
-    printf("enter limit\n");
-    scanf("%d",&limit);
+  printf("enter limit\n");
+  scanf("%d", &limit);
 
-    printf("enter your values\n");
-      for(i=0;i<limit;i++){
-       scanf("%d",&array[i]);
-          
-        }
-       for(i=limit-1;i>=0;i--){
-        
-        printf("%d\t",array[i]);
+  printf("enter your values\n");
+  for (i = 0; i < limit; i++)
+  {
+    scanf("%d", &array[i]);
+  }
 
-        
-       }
+  for (i = 0; i < limit / 2; i++)
+  {
+    temp = array[i];
+    array[i] = array[limit - i - 1];
+    array[limit - i - 1] = temp;
+  }
 
+  for (i = 0; i < limit; i++)
+  {
+    printf("%d\t", array[i]);
+  }
 
-      
-       
-       
-       
- return 0;
+  return 0;
 }
